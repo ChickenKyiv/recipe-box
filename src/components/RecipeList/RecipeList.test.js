b0025ea02bde964e13d7d0087dfb2c0cb342554d
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2019-03-04 22:13:28 +0200

    +

--- src/components/RecipeList/RecipeList.test.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import RecipeList from './RecipeList';
-//
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<RecipeList />, div);
-// });
